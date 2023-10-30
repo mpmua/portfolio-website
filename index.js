d4513b2ad7afdeeb051046e5d3093e9321281b7b
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-10-30 15:16:42 +0000

    react framework removed, website is now a standard html website

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const pageWrapElement = document.querySelector(".page-wrap");
 const singleAppCard = document.querySelector(".single-app-card");
 
+let isVisible = null;
+
 let options = {
   root: pageWrapElement,
   rootMargin: "0px",
   threshold: 1.0,
 };
 
+const callback = (entries) => {
+  isVisible = entries[0].isIntersecting;
+  console.log(entries[0]);
+  //   alert("Detected");
+};
+
 let observer = new IntersectionObserver(callback, options);
+
+observer.observe(singleAppCard);
