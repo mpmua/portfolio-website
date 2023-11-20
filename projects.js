d1303192b5df5fe3bc99ad9e9df63b430f6b8921
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-11-20 14:09:40 +0000

    project cards now being displayed via javascript

--- projects.js
+++ projects.js
@@ -1,3 +1,8 @@
+const browserIcon = "<i class='fa-brands fa-chrome'></i>";
+const appleIcon = "<i class='fa-brands fa-apple'></i>";
+const googleIcon = "<i class='fa-brands fa-google-play'></i>";
+const githubIcon = "<i class='fa-brands fa-github'></i>";
+
 const projectsArray = [
   {
     img: "/img/pw-showcase.png",
@@ -6,42 +11,70 @@ const projectsArray = [
     tech: ["HTML5", "CSS3", "Javascript", "Vite", "Capacitor"],
     linksArray: [
       {
-        link: "https://pure-weather.netlify.app/settings.html",
-        icon: "<FaFirefoxBrowser />",
+        link: "https://pure-weather.netlify.app/",
+        icon: browserIcon,
         text: "Live Demo",
       },
       {
-        link: "https://pure-weather.netlify.app/settings.html",
-        icon: "<i class='fa-brands fa-apple'></i>",
-        text: " Demo",
+        link: "https://github.com/mpmua/pure-weather",
+        icon: githubIcon,
+        text: "Source",
       },
       {
-        link: "https://pure-weather.netlify.app/settings.html",
-        icon: "<FaFirefoxBrowser />",
-        text: "lol Demo",
-      },
-      {
-        link: "https://pure-weather.netlify.app/settings.html",
-        icon: "<FaFirefoxBrowser />",
-        text: "Live Demo",
+        link: "https://play.google.com/store/apps/details/Pure_Weather?id=com.weatherapp.pure",
+        icon: googleIcon,
+        text: "Play Store",
       },
     ],
   },
   {
-    img: "/img/pw-showcase.png",
-    title: "Pure Bliss",
-    text: "An open source weather application that was built using HTML, CSS and Vanilla Javascript.",
-    tech: ["CSS3", "LOL", "Vite", "Capacitor"],
+    img: "/img/mta-showcase.png",
+    title: "My Tasbeeh App",
+    text: "An open source Tasbeeh app",
+    tech: ["HTML5", "CSS3", "Javascript", "Vite", "Capacitor"],
     linksArray: [
       {
-        link: "https://pure-weather.netlify.app/settings.html",
-        icon: "<FaFirefoxBrowser />",
-        text: "Hey Demo",
+        link: "https://mytasbeehapp.netlify.app/",
+        icon: browserIcon,
+        text: "Live Demo",
       },
       {
-        link: "https://pure-weather.netlify.app/settings.html",
-        icon: "<FaFirefoxBrowser />",
-        text: "no Demo",
+        link: "https://github.com/My-Ummah-Apps/my-tasbeeh-app",
+        icon: githubIcon,
+        text: "Source",
+      },
+      {
+        link: "https://apps.apple.com/in/app/my-tasbeeh-app/id6449438967",
+        icon: appleIcon,
+        text: "App Store",
+      },
+      {
+        link: "https://apps.apple.com/in/app/my-tasbeeh-app/id6449438967",
+        icon: googleIcon,
+        text: "Play Store",
+      },
+    ],
+  },
+  {
+    img: "/img/mgc-showcase.png",
+    title: "Mini Games Collection",
+    text: "Four mini games built using HTML, CSS and Vanilla Javascript.",
+    tech: ["HTML5", "CSS3", "Javascript", "Vite", "Capacitor"],
+    linksArray: [
+      {
+        link: "https://mini-games-collection.netlify.app/",
+        icon: browserIcon,
+        text: "Live Demo",
+      },
+      {
+        link: "https://github.com/mpmua/mini-games-collection",
+        icon: githubIcon,
+        text: "Source",
+      },
+      {
+        link: "https://play.google.com/store/apps/details?id=com.minigames.myapp",
+        icon: googleIcon,
+        text: "Play Store",
       },
     ],
   },
