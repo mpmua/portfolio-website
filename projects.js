a8ef9af28b8c0203a27052f046852ca24a161d02
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-04-26 16:11:11 +0100

    fix: fix play store link for salah app

--- projects.js
+++ projects.js
@@ -8,7 +8,7 @@ const projectsArray = [
     img: "./img/msa-showcase.png",
     title: "My Salah App",
     text: "An open source application which assists users in keeping track of their Prayers and gaining insights into their Prayer habits.",
-    tech: ["ReactJS", "Typescript", "Tailwind CSS", "Vite", "CapacitorJS"],
+    tech: ["ReactJS", "Typescript", "Tailwind CSS", "CapacitorJS"],
     linksArray: [
       {
         link: "https://mysalahapp.netlify.app/",
@@ -21,40 +21,40 @@ const projectsArray = [
         text: "Source",
       },
       {
-        link: "https://apps.apple.com/gb/app/my-salah-app/id6478277078",
-        icon: appleIcon,
-        text: "App Store",
-      },
-      {
         link: "https://play.google.com/store/apps/details?id=com.mysalahapp.app&hl=en-US",
         icon: googleIcon,
         text: "Play Store",
       },
+      {
+        link: "https://apps.apple.com/gb/app/my-salah-app/id6478277078",
+        icon: appleIcon,
+        text: "App Store",
+      },
     ],
   },
-  // {
-  //   img: "./img/pw-showcase.png",
-  //   title: "Pure Weather",
-  //   text: "An open source weather application which offers daily / hourly weather forecasts, weather alerts and dynamic theme changes based on live weather. it utilizes the OpenWeatherMap API.",
-  //   tech: ["HTML5", "CSS3", "Javascript", "Vite", "CapacitorJS"],
-  //   linksArray: [
-  //     {
-  //       link: "https://pure-weather.netlify.app/",
-  //       icon: browserIcon,
-  //       text: "Live Demo",
-  //     },
-  //     {
-  //       link: "https://github.com/mpmua/pure-weather",
-  //       icon: githubIcon,
-  //       text: "Source",
-  //     },
-  //   ],
-  // },
+  {
+    img: "./img/pw-showcase.png",
+    title: "Pure Weather",
+    text: "An open source weather application which offers daily / hourly weather forecasts, weather alerts and dynamic theme changes based on live weather. it utilizes the OpenWeatherMap API.",
+    tech: ["HTML5", "CSS3", "Javascript", "CapacitorJS"],
+    linksArray: [
+      {
+        link: "https://pure-weather.netlify.app/",
+        icon: browserIcon,
+        text: "Live Demo",
+      },
+      {
+        link: "https://github.com/mpmua/pure-weather",
+        icon: githubIcon,
+        text: "Source",
+      },
+    ],
+  },
   {
     img: "./img/mta-showcase.png",
     title: "My Tasbeeh App",
     text: "An open source Tasbeeh application. This application simplifies tally management by allowing users to effortlessly add, delete, and edit tallies, while offering options for a dark / light theme, daily counter resets, and daily notifications",
-    tech: ["HTML5", "CSS3", "Javascript", "Vite", "CapacitorJS"],
+    tech: ["ReactJS", "CapacitorJS"],
     linksArray: [
       {
         link: "https://mytasbeehapp.netlify.app/",
@@ -67,14 +67,14 @@ const projectsArray = [
         text: "Source",
       },
       {
-        link: "https://apps.apple.com/in/app/my-tasbeeh-app/id6449438967",
-        icon: appleIcon,
-        text: "App Store",
+        link: "https://play.google.com/store/apps/details?id=com.tasbeeh.my&hl=en-US",
+        icon: googleIcon,
+        text: "Play Store",
       },
       {
         link: "https://apps.apple.com/in/app/my-tasbeeh-app/id6449438967",
-        icon: googleIcon,
-        text: "Play Store",
+        icon: appleIcon,
+        text: "App Store",
       },
     ],
   },
@@ -82,7 +82,7 @@ const projectsArray = [
     img: "./img/mgc-showcase.png",
     title: "Mini Games Collection",
     text: "An application featuring four mini-games, storing user scores locally and updating high scores when required.",
-    tech: ["HTML5", "CSS3", "Javascript", "Vite", "CapacitorJS"],
+    tech: ["HTML5", "CSS3", "Javascript", "CapacitorJS"],
     linksArray: [
       {
         link: "https://mini-games-collection.netlify.app/",
@@ -93,11 +93,6 @@ const projectsArray = [
         link: "https://github.com/mpmua/mini-games-collection",
         icon: githubIcon,
         text: "Source",
-      },
-      {
-        link: "https://play.google.com/store/apps/details?id=com.minigames.myapp",
-        icon: googleIcon,
-        text: "Play Store",
       },
     ],
   },
