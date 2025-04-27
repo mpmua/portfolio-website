7b2b87452215a57c45b5aa731623e4a11676d52a
mpmua <39090870+mpmua@users.noreply.github.com>
2025-04-27 07:57:11 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -5,33 +5,38 @@ const githubIcon = "<i class='fa-brands fa-github'></i>";
 
 const projectsArray = [
   {
-    img: "/img/pw-showcase.png",
-    title: "Pure Weather",
-    text: "An open source weather application which offers daily / hourly weather forecasts, weather alerts and dynamic theme changes based on live weather. it utilizes the OpenWeatherMap API.",
-    tech: ["HTML5", "CSS3", "Javascript", "Vite", "CapacitorJS"],
+    img: "./img/msa-showcase.png",
+    title: "My Salah App",
+    text: "An open source application which assists users in keeping track of their Prayers and gaining insights into their Prayer habits.",
+    tech: ["ReactJS", "Typescript", "Tailwind CSS", "CapacitorJS"],
     linksArray: [
       {
-        link: "https://pure-weather.netlify.app/",
+        link: "https://mysalahapp.netlify.app/",
         icon: browserIcon,
         text: "Live Demo",
       },
       {
-        link: "https://github.com/mpmua/pure-weather",
+        link: "https://github.com/My-Ummah-Apps/My-Salah-App",
         icon: githubIcon,
         text: "Source",
       },
       {
-        link: "https://play.google.com/store/apps/details/Pure_Weather?id=com.weatherapp.pure",
+        link: "https://play.google.com/store/apps/details?id=com.mysalahapp.app&hl=en-US",
         icon: googleIcon,
         text: "Play Store",
+      },
+      {
+        link: "https://apps.apple.com/gb/app/my-salah-app/id6478277078",
+        icon: appleIcon,
+        text: "App Store",
       },
     ],
   },
   {
-    img: "/img/mta-showcase.png",
+    img: "./img/mta-showcase.png",
     title: "My Tasbeeh App",
     text: "An open source Tasbeeh application. This application simplifies tally management by allowing users to effortlessly add, delete, and edit tallies, while offering options for a dark / light theme, daily counter resets, and daily notifications",
-    tech: ["HTML5", "CSS3", "Javascript", "Vite", "CapacitorJS"],
+    tech: ["ReactJS", "CapacitorJS"],
     linksArray: [
       {
         link: "https://mytasbeehapp.netlify.app/",
@@ -44,22 +49,40 @@ const projectsArray = [
         text: "Source",
       },
       {
+        link: "https://play.google.com/store/apps/details?id=com.tasbeeh.my&hl=en-US",
+        icon: googleIcon,
+        text: "Play Store",
+      },
+      {
         link: "https://apps.apple.com/in/app/my-tasbeeh-app/id6449438967",
         icon: appleIcon,
         text: "App Store",
       },
+    ],
+  },
+  {
+    img: "./img/pw-showcase.png",
+    title: "Pure Weather",
+    text: "An open source weather application which offers daily / hourly weather forecasts, weather alerts and dynamic theme changes based on live weather. it utilizes the OpenWeatherMap API.",
+    tech: ["HTML5", "CSS3", "Javascript", "CapacitorJS"],
+    linksArray: [
       {
-        link: "https://apps.apple.com/in/app/my-tasbeeh-app/id6449438967",
-        icon: googleIcon,
-        text: "Play Store",
+        link: "https://pure-weather.netlify.app/",
+        icon: browserIcon,
+        text: "Live Demo",
+      },
+      {
+        link: "https://github.com/mpmua/pure-weather",
+        icon: githubIcon,
+        text: "Source",
       },
     ],
   },
   {
-    img: "/img/mgc-showcase.png",
+    img: "./img/mgc-showcase.png",
     title: "Mini Games Collection",
     text: "An application featuring four mini-games, storing user scores locally and updating high scores when required.",
-    tech: ["HTML5", "CSS3", "Javascript", "Vite", "CapacitorJS"],
+    tech: ["HTML5", "CSS3", "Javascript", "CapacitorJS"],
     linksArray: [
       {
         link: "https://mini-games-collection.netlify.app/",
@@ -71,15 +94,10 @@ const projectsArray = [
         icon: githubIcon,
         text: "Source",
       },
-      {
-        link: "https://play.google.com/store/apps/details?id=com.minigames.myapp",
-        icon: googleIcon,
-        text: "Play Store",
-      },
     ],
   },
   {
-    img: "/img/portfolio-showcase.png",
+    img: "./img/portfolio-showcase.png",
     title: "Portfolio",
     text: "This page! Responsive website built with HTML, Tailwind CSS and Javascript. Each project is stored as an an individual object within a seperate javascript file and then auto-generated into html for easy maintenance.",
     tech: ["HTML5", "Tailwind CSS", "Javascript"],
